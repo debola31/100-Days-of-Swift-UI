49cf9785991520b2ebbd6959c9422afde54043ca
Debo Akeredolu <aakeredolu@apple.com>
2023-08-15 23:17:47 -0400

    day 38

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by ADEBOLA AKEREDOLU on 8/12/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

21adc65eba1dfae9eb7ce85832cfe433ca9ffaf6
aakeredolu <aakeredolu@apple.com>
2023-08-19 12:32:17 -0700

    day 53

--- Bookworm/Bookworm/DataController.swift
+++ Bookworm/Bookworm/DataController.swift
@@ -5,4 +5,17 @@
 //  Created by ADEBOLA AKEREDOLU on 8/19/23.
 //
 
+import CoreData
 import Foundation
+
+class DataContoller: ObservableObject {
+    let container = NSPersistentContainer(name: "Bookworm")
+    
+    init () {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print ("Core Data failed to load: \(error.localizedDescription)")
+            }
+        }
+    }
+}

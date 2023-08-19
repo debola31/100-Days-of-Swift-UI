4d98f19fe1178a82b46d0165bda82cc70cdc6baf
aakeredolu <aakeredolu@apple.com>
2023-08-19 12:32:13 -0700

    day 53 done

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by ADEBOLA AKEREDOLU on 8/19/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bookworm/Bookworm/DataController.swift
@@ -0,0 +1,8 @@
+//
+//  DataController.swift
+//  Bookworm
+//
+//  Created by ADEBOLA AKEREDOLU on 8/19/23.
+//
+
+import Foundation

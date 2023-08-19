4f0ef5caea61de3bf9299cef13ebc7a594239f77
aakeredolu <aakeredolu@apple.com>
2023-08-19 10:54:20 -0700

    cupcake pre challenge

--- CupcakeCorner/CupcakeCorner/Model.swift
+++ CupcakeCorner/CupcakeCorner/Model.swift
@@ -6,3 +6,55 @@
 //
 
 import Foundation
+
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
+class User: ObservableObject, Codable {
+    @Published var name = "Paul Hudson"
+    @Published var help = "Sule"
+
+    enum CodingKeys: CodingKey {
+        case name
+        case help
+    }
+
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+        help = try container.decode(String.self, forKey: .help)
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+        try container.encode(help, forKey: .help)
+    }
+}
+
+func loadData() async -> [Result] {
+    var results = [Result]()
+    
+    guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+        print("Invalid URL")
+        return []
+    }
+
+    do {
+        let (data, _) = try await URLSession.shared.data(from: url)
+        if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+            results = decodedResponse.results
+            return results
+        }
+    } catch {
+        print("Invalid data")
+    }
+    return []
+}
